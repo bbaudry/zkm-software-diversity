45164fadceffb85e7c454d8213201a6785d39d3c
bbaudry <baudry@kth.se>
2025-06-23 09:22:28 +0200

    nannou

--- desordrerougenannou/src/main.rs
+++ desordrerougenannou/src/main.rs
@@ -15,7 +15,13 @@ fn view(app: &App, frame: Frame) {
     let window = app.main_window();
     window.set_inner_size_pixels(900,900);
     let draw = app.draw();
-    draw.background().color(PLUM);
+    draw.background().color(WHITE);
     draw.ellipse().color(STEELBLUE);
+    draw.line()
+        .start(pt2(-100.0,0.0))
+        .end(pt2(100.0,0.0))
+        .weight(1.23)
+        .color(hsl(0.0,1.0,0.5));
+
     draw.to_frame(app, &frame).unwrap();
 }
